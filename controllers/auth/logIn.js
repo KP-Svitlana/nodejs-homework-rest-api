9bf5d0d55077497505f9ec35ef3f6f1992ecb019
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2023-05-06 19:51:16 +0300

    added token

--- controllers/auth/logIn.js
+++ controllers/auth/logIn.js
@@ -1,6 +1,10 @@
 const User = require("../../models/user");
 const { HttpError, ctrlWrapper } = require("../../helpers");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
 
 const logIn = async (req, res) => {
   const { email, password } = req.body;
@@ -14,7 +18,11 @@ const logIn = async (req, res) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
-  const token = "74grfcidfbfkgh";
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
 
   res.json({
     token,
